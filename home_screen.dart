29cabde53ea9bfa642db30f96b2c67858f5cacd3
Ahzfikar21 <101957654+Ahzfikar21@users.noreply.github.com>
2022-05-29 13:05:49 +0800

    Home Screen

--- home_screen.dart
+++ home_screen.dart
@@ -1,13 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_order_project/models/menu.dart';
 import 'package:food_order_project/screens/login_screen.dart';
-import '../theme.dart';
+import 'package:food_order_project/theme.dart';
+import 'package:food_order_project/widgets/menu_card.dart';
 
 class HomeScreen extends StatelessWidget {
   // const name ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference products = firestore.collection('products');
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -15,108 +20,71 @@ class HomeScreen extends StatelessWidget {
             vertical: 32,
             horizontal: 20,
           ), //EdgeInsets.symmetric
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hello, Fikar',
-                style: poppinsTextStyle.copyWith(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                  color: blackColor,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello, Fikar',
+                  style: poppinsTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: blackColor,
+                  ),
                 ),
-              ),
-              Text(
-                'Welcome To Yummy Food',
-                style: poppinsTextStyle.copyWith(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: greyColor,
-                ),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              Text('Recommended Menu',
+                Text(
+                  'Welcome To Yummy Food',
                   style: poppinsTextStyle.copyWith(
                     fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                    color: blackColor,
-                  )),
-              SizedBox(
-                height: 32,
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    'assets/images/pic1.png',
-                    width: 130,
-                    height: 110,
+                    fontWeight: FontWeight.w500,
+                    color: greyColor,
                   ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/images/promo.png',
-                          width: 47,
-                          height: 16,
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Text(
-                          'Zamburger',
-                          style: poppinsTextStyle.copyWith(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: blackColor,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              'RM 15',
-                              style: poppinsTextStyle.copyWith(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                color: greyColor,
-                                decoration: TextDecoration.lineThrough,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              'RM 8',
-                              style: poppinsTextStyle.copyWith(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                color: yellowColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Text(
-                          'Free Delivery',
-                          style: poppinsTextStyle.copyWith(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w300,
-                            color: greyColor,
-                          ),
-                        ),
-                      ]),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Text('Recommended Menu',
+                    style: poppinsTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: blackColor,
+                    )),
+                SizedBox(
+                  height: 32,
+                ),
+                StreamBuilder<QuerySnapshot>(
+                    stream:
+                        products.orderBy('id', descending: false).snapshots(),
+                    builder: (_, snapshot) {
+                      {
+                        if (snapshot.hasData) {
+                          return Column(
+                            // ignore: unnecessary_cast
+                            children: (snapshot.data! as QuerySnapshot)
+                                .docs
+                                .map(
+                                  (e) => MenuCard(
+                                    Menu(
+                                      id: e['id'],
+                                      image: e['image'],
+                                      name: e['name'],
+                                      price: e['price'],
+                                      pricepromo: e['pricePromo'],
+                                      note: e['note'],
+                                      isPromo: e['isPromo'],
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                          );
+                        } else {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                      }
+                    }),
+              ],
+            ),
           ),
         ),
       ),
